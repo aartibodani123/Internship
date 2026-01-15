242337ad278112ec57a62908a97466f7751a77a9
Aarti Bodani <aartibodani@vasyerp.com>
2026-01-15 14:50:44 +0530

    Complete database implementation in simplewebapp

--- simplewebapp/src/main/java/com/example/simplewebapp/model/Product.java
+++ simplewebapp/src/main/java/com/example/simplewebapp/model/Product.java
@@ -1,13 +1,21 @@
 package com.example.simplewebapp.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Component;
 
+
+@Entity
 @Data
 @AllArgsConstructor
 public class Product {
+
+    @Id
     private int prodId;
+
     private String prodName;
     private int price;
 
